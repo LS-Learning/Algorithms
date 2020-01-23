0309df4b0399186901d0a8024e7b3605846b1a79
91a3b0 <elliot.j.gunn@gmail.com>
2020-01-23 18:45:23 -0500

    stock_prices done

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -15,13 +15,17 @@ def find_max_profit(prices):
         print(temp_list)
 
         # loop through temp_list, in reverse
-        for x in range(1, len(temp_list), -1):
+        for x in range(len(temp_list)-1, 1, -1):
           profit = temp_list[-1] - temp_list[x-1]
           print(x, profit)
           if profit > max_profit:
             max_profit = profit
+            print(max_profit)
     
     return max_profit
 
 l = [10, 7, 5, 8, 11, 9]
 print(find_max_profit(l))
+
+for x in range(3, 1, -1):
+  print(x)
