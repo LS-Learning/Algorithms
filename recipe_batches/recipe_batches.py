032ce5f3fa12c9932a919e8a2b76f1e4058b10d1
91a3b0 <elliot.j.gunn@gmail.com>
2020-01-23 17:46:56 -0500

    recipe_batches done

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,15 @@
 
 import math
 
+recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    if len(recipe.keys()) != len(ingredients.keys()):
+        return 0
+    else:
+        dict = {key: ingredients[key] / recipe[key] for key in ingredients if key in recipe}
+        return min(dict.values())
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+print(recipe_batches(recipe, ingredients))
